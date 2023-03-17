1155c0e900991df7bf613017ee9cd549761418bc
RodrigoCantarelli <128100257+RodrigoCantarelli@users.noreply.github.com>
2023-03-16 22:11:10 -0300

    Create exercicio05.py

---
+++ semana01/exercicio05.py
@@ -0,0 +1,7 @@
+### Exercicio 5 ### 
+numero = float(input("Digite o número: "))
+
+dobro = numero * 2 
+print("O dobro do seu número é: ", dobro)
+metade = numero / 2
+print("A metade do seu número é: ", metade)
