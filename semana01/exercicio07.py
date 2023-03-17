fb3786967020c10097ef08faea7c139986e71ad8
RodrigoCantarelli <128100257+RodrigoCantarelli@users.noreply.github.com>
2023-03-16 22:19:04 -0300

    Create exercicio07.py

---
+++ semana01/exercicio07.py
@@ -0,0 +1,6 @@
+### Exercicio 7 ###
+qCarros = int(input("Digite a quantidade de carros total: "))
+qPneus = qCarros * 4
+
+valorFinal = qPneus * 395.40
+print("O valor total para a troca de todos os pneus da frota será de R$", valorFinal)
