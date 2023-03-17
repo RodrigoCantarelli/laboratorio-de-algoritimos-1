6c0737c9e9dff85f771190c3dadc494c0a3e4abd
RodrigoCantarelli <128100257+RodrigoCantarelli@users.noreply.github.com>
2023-03-16 22:03:55 -0300

    Create exercicio03.py

---
+++ semana01/exercicio03.py
@@ -0,0 +1,7 @@
+### Exercicio 3 ###
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade: "))
+sexo = input("Digite o seu sexo: ")
+curso = input("Digite o seu curso: ")
+
+print("Olá, meu nome é ", nome, "sou do sexo ", sexo, "tenho ", idade, "anos, e estudo ", curso)
