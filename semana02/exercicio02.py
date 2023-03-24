89d5d856de49cdbb2d4d312f8ebe9700ab36bf02
RodrigoCantarelli <128100257+RodrigoCantarelli@users.noreply.github.com>
2023-03-23 21:46:45 -0300

    Create exercicio02.py

---
+++ semana02/exercicio02.py
@@ -0,0 +1,10 @@
+### Leia dois números e apresente em ordem crescente ###
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+if n1 < n2:
+    print(n1, n2)
+    
+else:
+    print(n2, n1)
