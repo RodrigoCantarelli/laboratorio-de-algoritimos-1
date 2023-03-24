e1376d12ab201fa8b14734b626dee3fbec58b7fa
RodrigoCantarelli <128100257+RodrigoCantarelli@users.noreply.github.com>
2023-03-23 21:48:28 -0300

    Create exercicio05.py

---
+++ semana02/exercicio05.py
@@ -0,0 +1,8 @@
+### Escreva um algoritmo em Python que dada a idade de uma pessoa, determine sua classificação:###
+
+idade = int(input("Digite a sua idade atual: "))
+
+if idade >= 18:
+    print("Você é maior de idade!!")
+else:
+    print("Você é menor de idade!!")
